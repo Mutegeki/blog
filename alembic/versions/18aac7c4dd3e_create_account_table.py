62afccfcc79c693a4e0e6f04e4d7ab8cc09c7770
Mutegeki <braynmute@gmail.com>
2018-10-25 07:27:35 +0300

    Added alembic files

---
+++ alembic/versions/18aac7c4dd3e_create_account_table.py
@@ -0,0 +1,24 @@
+"""create account table
+
+Revision ID: 18aac7c4dd3e
+Revises: 
+Create Date: 2018-10-24 18:51:21.162427
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '18aac7c4dd3e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
